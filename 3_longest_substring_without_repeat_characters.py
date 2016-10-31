e2f528ebd9c75e85074f414b9318fdb8cd05cc20
Lu Zhou <zhoulu@qinggukeji.com>
2016-10-31 16:45:47 -0700

    3 longest substring without repeat characters

---
+++ 3_longest_substring_without_repeat_characters.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_to_idx = {}
+        max_length = 0
+        curr_length = 0
+        s_idx = -1
+        for i, c in enumerate(s):
+            if c in char_to_idx and char_to_idx[c] > s_idx:
+                if curr_length > max_length:
+                    max_length = curr_length
+                curr_length = i - char_to_idx[c]
+                s_idx = char_to_idx[c]
+            else:
+                curr_length += 1
+            char_to_idx[c] = i
+        if curr_length > max_length:
+            max_length = curr_length
+            
+        return max_length
