b40b24d42972bf1e53beb896095db358d1f06774
zhoulu312 <zhoulu312@gmail.com>
2016-11-15 22:40:56 -0800

    Update 5_longest_palindromic_substring.py

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -1,4 +1,5 @@
 # percentage: 1.26%
+# DP solution, O(n^2)
 class Solution(object):
     def longestPalindrome(self, s):
         """
@@ -8,15 +9,16 @@ class Solution(object):
         p_map = [[False] * len(s) for _ in range(len(s))]
         max_i = 0
         max_j = 0
+        # 1 char substring.
         for i in range(len(s)):
             p_map[i][i] = True
-            
+        # 2 char substring
         for i in range(1, len(s)):
             if s[i] == s[i-1]:
                 p_map[i][i-1] = True
                 max_i = i - 1
                 max_j = i
-        
+        # Everything else.
         for offset in range(1, len(s)):
             for i in range(len(s) - offset):
                 j = i + offset
