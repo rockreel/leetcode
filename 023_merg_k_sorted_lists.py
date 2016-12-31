90eab7a2bc0fffa0c39a514a0d80514027db253e
Lu Zhou <zhoulu@qinggukeji.com>
2016-12-30 20:25:53 -0800

    23

---
+++ 023_merg_k_sorted_lists.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        import heapq
+        head = ListNode(None)
+        p = head
+        head_nodes = [(l.val, l) for l in lists if l]
+        heapq.heapify(head_nodes)
+        while head_nodes:
+            _, n = heapq.heappop(head_nodes)
+            if n.next:
+                heapq.heappush(head_nodes, (n.next.val, n.next))
+            p.next = n
+            p = p.next
+        return head.next
