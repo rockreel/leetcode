16d3605c967aff9459600fd0523608a594c6c14a
Lu Zhou <zhoulu312@gmail.com>
2017-01-03 16:31:01 -0800

    44

--- 044_wildcard_matching.py
+++ 044_wildcard_matching.py
@@ -1,3 +1,33 @@
+# DP solution.
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """ 
+        dp = [
+            [None] * (len(s)+1) for _ in range(len(p)+1)
+        ]
+        
+        # Initializae solution matrix.
+        dp[0][0] = True  # Empty p match empty s.
+        for j in range(1, len(s)+1):
+            # Empty p can not match non-empty s.
+            dp[0][j] = False
+        for i in range(1, len(p)+1):
+            # Empty s can still match if current c in p is * and also match previously.
+            dp[i][0] = (p[i-1] == '*' and dp[i-1][0])
+            
+        for i in range(1, len(p)+1):
+            for j in range(1, len(s)+1):
+                if p[i-1] != '*':
+                    dp[i][j] = (p[i-1] == s[j-1] or p[i-1] == '?') and dp[i-1][j-1]
+                else:
+                    dp[i][j] = (dp[i-1][j] or  # Not-including * already match whole s up to j.
+                                dp[i][j-1])     # Including * match s up to j-1.
+        return dp[-1][-1]
+
 # Recursive solution.
 class Solution(object):
     def isMatch(self, s, p):
