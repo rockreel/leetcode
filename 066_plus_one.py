5cfe254c38773eb41f1c94387addfbe90fe71ddd
Lu Zhou <zhoulu312@gmail.com>
2017-01-06 19:16:30 -0800

    66

---
+++ 066_plus_one.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                break
+        else:
+            digits.insert(0, 1)
+                
+        return digits
+
