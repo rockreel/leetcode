151765fb7531a0aded1d05deb591943296001e30
Lu Zhou <zhoulu312@gmail.com>
2017-01-16 19:16:47 -0800

    130

--- 130_surrounded_regions.py
+++ 130_surrounded_regions.py
@@ -4,53 +4,40 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
+        def mark_board(board, i, j, visited):
+            # Mark connected O cells.
+            queue = [(i, j)]
+            while queue:
+                row, col = queue.pop(0)
+                if (row, col) in visited:
+                    continue
+                board[row][col] = 'R'
+                visited.add((row, col))
+                if row > 0 and board[row-1][col] == 'O':
+                    queue.append((row-1, col))
+                if row < len(board)-1 and board[row+1][col] == 'O':
+                    queue.append((row+1, col))
+                if col > 0 and board[row][col-1] == 'O':
+                    queue.append((row, col-1))
+                if col < len(board[0])-1 and board[row][col+1] == 'O':
+                    queue.append((row, col+1))
+        
         if not board or not board[0]:
             return 
-        # From top to bottom.
+        visited = set()
+        # Start from O on left and right side.
         for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == 'O' and (i == 0 or board[i-1][j] == 'R'):
-                    board[i][j] = 'R'
-            for j in range(len(board[0])-1):               
-                if board[i][j] == 'R' and board[i][j+1] == 'O':
-                    board[i][j+1] = 'R'
-            for j in range(len(board[0])-1, 0, -1):
-                if board[i][j] == 'R' and board[i][j-1] == 'O':
-                    board[i][j-1] = 'R'
-        # From bottom to top    
-        for i in range(len(board)-1, -1, -1):
-            for j in range(len(board[0])):
-                if board[i][j] == 'O' and (i == len(board)-1 or board[i+1][j] == 'R'):
-                    board[i][j] = 'R'
-            for j in range(len(board[0])-1):
-                if board[i][j] == 'R' and board[i][j+1] == 'O':
-                    board[i][j+1] = 'R'
-            for j in range(len(board[0])-1, 0, -1):
-                if board[i][j] == 'R' and board[i][j-1] == 'O':
-                    board[i][j-1] = 'R'
-        # From left to right.             
+            if board[i][0] == 'O':
+                mark_board(board, i, 0, visited)
+            if board[i][len(board[0])-1] == 'O':
+                mark_board(board, i, len(board[0])-1, visited)
+        # Start from O on top and bottom side.
         for j in range(len(board[0])):
-            for i in range(len(board)):
-                if board[i][j] == 'O' and (j == 0 and board[i][j-1] == 'R'):
-                    board[i][j] = 'R'
-            for i in range(len(board)-1):
-                if board[i][j] == 'R' and board[i+1][j] == 'O':
-                    board[i+1][j] = 'R'
-            for i in range(len(board)-1, 0, -1):
-                if board[i][j] == 'R' and board[i-1][j] == 'O':
-                    board[i-1][j] = 'R'
-        # From right to left.
-        for j in range(len(board[0])-1, -1, -1):
-            for i in range(len(board)):
-                if board[i][j] == 'O' and (j == len(board[0])-1 and board[i][j+1] == 'R'):
-                    board[i][j] = 'R'
-            for i in range(len(board)-1):
-                if board[i][j] == 'R' and board[i+1][j] == 'O':
-                    board[i+1][j] = 'R'
-            for i in range(len(board)-1, 0, -1):
-                if board[i][j] == 'R' and board[i-1][j] == 'O':
-                    board[i-1][j] = 'R'
-        # Mark.
+            if board[0][j] == 'O':
+                mark_board(board, 0, j, visited)
+            if board[len(board)-1][j] == 'O':
+                mark_board(board, len(board)-1, j, visited)
+        # Remark cells.
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == 'R':
