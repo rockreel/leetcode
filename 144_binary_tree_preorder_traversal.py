df15c72985b47752e3704cb0a131d1a715fbe1d2
Lu Zhou <zhoulu312@gmail.com>
2017-01-22 14:44:21 -0800

    144

--- 144_binary_tree_preorder_traversal.py
+++ 144_binary_tree_preorder_traversal.py
@@ -5,21 +5,47 @@
 #         self.left = None
 #         self.right = None
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
     def preorderTraversal(self, root):
         """
         :type root: TreeNode
         :rtype: List[int]
         """
-        def traverse(root, nodes):
-            if not root:
-                return
-            nodes.append(root.val)
-            if root.left:
-                traverse(root.left, nodes)
-            if root.right:
-                traverse(root.right, nodes)
+        if not root:
+            return []
+        stack = [root]
         nodes = []
-        traverse(root, nodes)
+        while stack:
+            node = stack.pop()
+            nodes.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
         return nodes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
