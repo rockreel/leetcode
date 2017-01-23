776efb84ae7e17a8a8a3c5b1f186fae0ebcf953e
Lu Zhou <zhoulu312@gmail.com>
2017-01-22 21:07:06 -0800

    154

--- 154_find_minimum_in_rotated_sorted_array_2.py
+++ 154_find_minimum_in_rotated_sorted_array_2.py
@@ -6,16 +6,12 @@ class Solution(object):
         """
         start, end = 0, len(nums)-1
         while start < end - 1:
-            if nums[start] < nums[end]:  # No rotation from start to end.
-                return nums[start]
-                
             middle = (start+end)/2
-            if nums[middle] > nums[start]:
+            if nums[middle] > nums[end]:
                 start = middle
             elif nums[middle] < nums[end]:
                 end = middle
             else:
-                start += 1
-                
+                end -= 1
         return min(nums[start], nums[end])
 
