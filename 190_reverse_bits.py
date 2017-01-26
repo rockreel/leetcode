334296ce7ad02b8211fca3a0e9ad04e1db96fde6
Lu Zhou <zhoulu312@gmail.com>
2017-01-25 18:54:49 -0800

    190

---
+++ 190_reverse_bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        result = 0
+        for i in range(32):
+            result |= (n & 1) << (31 - i)
+            n = n >> 1
+        return result
+
