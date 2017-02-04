0e1978a71e19576c099e3d517f63bcb3c52ac912
Lu Zhou <zhoulu312@gmail.com>
2017-02-03 23:55:09 -0800

    258

--- 258_add_digits.py
+++ 258_add_digits.py
@@ -15,3 +15,13 @@ class Solution(object):
             num = add_digits(num)
         return num
 
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num < 10:
+            return num
+        return num-9*((num-1)/9)
+
