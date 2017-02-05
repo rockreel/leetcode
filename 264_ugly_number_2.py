fdac91d6fa13063fa20dc5e5468ad130bc5b0484
Lu Zhou <zhoulu312@gmail.com>
2017-02-04 18:54:43 -0800

    264

--- 264_ugly_number_2.py
+++ 264_ugly_number_2.py
@@ -1,3 +1,26 @@
+# Use an array.
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        nums = [1]
+        i2, i3, i5 = 0, 0, 0
+        while len(nums) < n:
+            m2 = nums[i2] * 2
+            m3 = nums[i3] * 3
+            m5 = nums[i5] * 5
+            m = min(m2, m3, m5)
+            nums.append(m)
+            if m == m2:
+                i2 += 1
+            if m == m3:
+                i3 += 1
+            if m == m5:
+                i5 += 1
+        return nums[-1]
+
 # Use a heap.
 class Solution(object):
     def nthUglyNumber(self, n):
