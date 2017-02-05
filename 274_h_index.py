5d328f65900c2ffd4664beb30a8289438c69804b
Lu Zhou <zhoulu312@gmail.com>
2017-02-05 11:10:55 -0800

    274

--- 274_h_index.py
+++ 274_h_index.py
@@ -1,3 +1,4 @@
+# O(nlogn), sort solution.
 class Solution(object):
     def hIndex(self, citations):
         """
@@ -10,3 +11,24 @@ class Solution(object):
                 return i 
         return len(citations)
 
+# O(n), count solution.
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        # Count citation by hindex.
+        citation_by_hindex = [0] * (len(citations) + 1)
+        for c in citations:
+            if c < len(citation_by_hindex):
+                citation_by_hindex[c] += 1
+            else:
+                citation_by_hindex[-1] += 1
+        # Iterate from high to low find max hindex.
+        num_total_citations = 0
+        for h in range(len(citation_by_hindex)-1, -1, -1):
+            num_total_citations += citation_by_hindex[h]
+            if h <= num_total_citations:
+                return h
+        return 0
