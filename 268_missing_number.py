c62dcc259cee0225ff03689fb4aee81bffa2f2e8
Lu Zhou <zhoulu312@gmail.com>
2017-02-04 23:00:21 -0800

    268

---
+++ 268_missing_number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return len(nums)*(len(nums)+1)/2-sum(nums)
+
