eaffbfe2271c372abe3bce219432ff4fa0da21ce
Lu Zhou <zhoulu312@gmail.com>
2017-03-01 19:45:05 -0800

    343

---
+++ 343_integer_break.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+        result = 1
+        while n > 4:
+            result *= 3
+            n -= 3
+        return result * n
+
