741fd684c6a73210200697fdebee13d6fed7397b
Lu Zhou <zhoulu312@gmail.com>
2017-03-04 22:46:44 -0800

    365

---
+++ 365_water_and_jug_problem.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canMeasureWater(self, x, y, z):
+        """
+        :type x: int
+        :type y: int
+        :type z: int
+        :rtype: bool
+        """
+        def gcd(x, y):
+            return x if y == 0 else gcd(y, x % y)
+            
+        return z == 0 or (z <= x + y and z % gcd(x, y) == 0)
+
