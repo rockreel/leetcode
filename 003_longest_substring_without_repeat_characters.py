3b40cd9500ab0fead5a2ff926527209e8a2114ef
Lu Zhou <zhoulu312@gmail.com>
2017-10-08 05:04:51 +0000

    003_longest_substring_without_repeat_characters.py edited online with Bitbucket

--- 003_longest_substring_without_repeat_characters.py
+++ 003_longest_substring_without_repeat_characters.py
@@ -4,17 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        char_to_idx = {}
-        curr_len = 0
+        c_to_idx = {}
         max_len = 0
-        s_idx = -1
+        start = 0
         for i, c in enumerate(s):
-            if c in char_to_idx and char_to_idx[c] > s_idx:
-                curr_len = i - char_to_idx[c]
-                s_idx = char_to_idx[c]
-            else:
-                curr_len += 1
-            max_len = max(curr_len, max_len)
-            char_to_idx[c] = i
-        return max(curr_len, max_len)
-
+            if c in c_to_idx and c_to_idx[c] >= start:
+                start = c_to_idx[c] + 1
+            c_to_idx[c] = i
+            max_len = max(max_len, i - start + 1)
+        return max_len
