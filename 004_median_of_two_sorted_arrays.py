415c1fc4717707b46b4ef06b6dae48916c515714
Lu Zhou <zhoulu312@gmail.com>
2017-10-09 01:51:41 +0000

    004_median_of_two_sorted_arrays.py edited online with Bitbucket

--- 004_median_of_two_sorted_arrays.py
+++ 004_median_of_two_sorted_arrays.py
@@ -1,5 +1,3 @@
-# percentage: 38.26%
-# Log n solution.
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -7,33 +5,24 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        merged_list = []
-        if not nums1:
-            merged_list = nums2
-        elif not nums2:
-            merged_list = nums1
-        else:
-            idx1 = 0
-            idx2 = 0
-            while idx1 < len(nums1) or idx2 < len(nums2):
-                if idx1 == len(nums1):
-                    merged_list.append(nums2[idx2])
-                    idx2 += 1
-                elif idx2 == len(nums2):
-                    merged_list.append(nums1[idx1])
-                    idx1 += 1
-                else:
-                    if nums1[idx1] < nums2[idx2]:
-                        merged_list.append(nums1[idx1])
-                        idx1 += 1
-                    else:
-                        merged_list.append(nums2[idx2])
-                        idx2 += 1
-
-        l = len(merged_list)
-        if l % 2 == 0:
-            return (merged_list[l/2-1] + merged_list[l/2])/2.0
-        else:
-            return merged_list[l/2]
+        import sys
+        def findKthElement(nums1, nums2, k):
+            if not nums1:
+                return nums2[k-1]
+            if not nums2:
+                return nums1[k-1]
+            if k == 1:
+                return min(nums1[0], nums2[0])
             
-        
+            m1 = nums1[k/2-1] if k/2 - 1 < len(nums1) else sys.maxint
+            m2 = nums2[k/2-1] if k/2 - 1 < len(nums2) else sys.maxint
+            if m1 < m2:
+                return findKthElement(nums1[k/2:], nums2, k - k/2 )
+            else:
+                return findKthElement(nums1, nums2[k/2:], k - k/2 )
+
+        k = len(nums1) + len(nums2)
+        if k % 2 == 0:
+            return (findKthElement(nums1, nums2, k/2) + findKthElement(nums1, nums2, k/2+1)) / 2.0
+        else:
+            return findKthElement(nums1, nums2, k/2+1)
