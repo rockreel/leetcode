573c4b4977aebb67eb8153c0052dddd1d1614f45
Lu Zhou <zhoulu312@gmail.com>
2017-10-09 05:15:54 +0000

    005_longest_palindromic_substring.py edited online with Bitbucket

--- 005_longest_palindromic_substring.py
+++ 005_longest_palindromic_substring.py
@@ -1,32 +1,24 @@
-# percentage: 1.26%
-# DP solution, O(n^2)
 class Solution(object):
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        p_map = [[False] * len(s) for _ in range(len(s))]
-        max_i = 0
-        max_j = 0
-        # 1 char substring.
-        for i in range(len(s)):
-            p_map[i][i] = True
-        # 2 char substring
-        for i in range(1, len(s)):
-            if s[i] == s[i-1]:
-                p_map[i][i-1] = True
-                max_i = i - 1
-                max_j = i
-        # Everything else.
-        for offset in range(1, len(s)):
-            for i in range(len(s) - offset):
-                j = i + offset
-                if p_map[i+1][j-1] and s[i] == s[j]:
-                    p_map[i][j] = True
-                    if max_j - max_i + 1 < j - i + 1:
-                        max_i = i
-                        max_j = j
+        substr = s[0] if s else ''
+        
+        # If s[i:j] is palindrome.
+        dp = [[False] * (len(s) + 1) for i in range(len(s) + 1)]
+        for i in range(len(s) + 1):
+            dp[i][i] = True
+            dp[i][min(i+1, len(s))] = True
 
-        return s[max_i:max_j+1]
-    
+        # Traverse diagonally.
+        for offset in range(2, len(s) + 1):
+            for i in range(len(s) - offset + 1):
+                j = i + offset
+                if dp[i+1][j-1] and s[i] == s[j-1]:
+                    dp[i][j] = True
+                    if j - i > len(substr):
+                        substr = s[i:j]
+
+        return substr
