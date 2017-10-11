c07a9bea3dc7360708b5a0d07662aa5d742607d9
Lu Zhou <zhoulu312@gmail.com>
2017-10-11 06:06:05 +0000

    009_palindrome_number.py edited online with Bitbucket

--- 009_palindrome_number.py
+++ 009_palindrome_number.py
@@ -1,24 +1,12 @@
-# percentage: 32.09%
 class Solution(object):
     def isPalindrome(self, x):
         """
         :type x: int
         :rtype: bool
         """
-        if x  < 0:
-            return False
-            
-        digits = []
-        while x > 0:
-            digits.append(x%10)
-            x /= 10
-        
-        i, j = 0, len(digits) - 1
-        
-        while i < j:
-            if digits[i] != digits[j]:
-                return False
-            i += 1
-            j -= 1
-                
-        return True
+        n = abs(x)
+        r = 0
+        while n > 0:
+            r = 10 * r + n % 10
+            n /= 10
+        return r == x
