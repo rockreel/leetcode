61a806e23ad25c3fd49f3d9dccf66a05ce446032
lu.zhou <lu.zhou@workday.com>
2019-12-06 15:46:54 -0800

    0007 java

---
+++ java/src/test/java/leetcode/L0007ReverseIntegerTest.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class L0007ReverseIntegerTest {
+
+  final L0007ReverseInteger solution = new L0007ReverseInteger();
+
+  @Test
+  public void test() {
+    assertEquals(321, solution.reverse(123));
+    assertEquals(-321, solution.reverse(-123));
+    assertEquals(21, solution.reverse(120));
+    assertEquals(0, solution.reverse(1534236469));
+    assertEquals(0, solution.reverse(-2147483648));
+  }
+
+}
