63d10d4f4a613f88366a081ed0b2b86fe4c8bdda
Lu Zhou <zhoulu312@gmail.com>
2019-12-06 00:35:38 +0000

    005_longest_palindromic_substring.py edited online with Bitbucket

--- python2/005_longest_palindromic_substring.py
+++ python2/005_longest_palindromic_substring.py
@@ -23,3 +23,13 @@ class Solution(object):
                         substr = s[i:j]
 
         return substr
+        
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        max_sub = ''
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                sub = s[i:j+1]
+                if sub == sub[::-1] and len(sub) > len(max_sub):
+                    max_sub = sub
+        return max_sub
