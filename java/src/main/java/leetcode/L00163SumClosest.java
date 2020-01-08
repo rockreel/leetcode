7618cb411746f1340d802a43553b81a20cfead56
lu.zhou <lu.zhou@workday.com>
2020-01-08 11:03:51 -0800

    0016 java

---
+++ java/src/main/java/leetcode/L00163SumClosest.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class L00163SumClosest {
+
+  public int threeSumClosest(int[] nums, int target) {
+    return 2;
+  }
+
+}
---
+++ java/src/test/java/leetcode/L00163SumClosestTest.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class L00163SumClosestTest {
+
+  L00163SumClosest solution = new L00163SumClosest();
+
+  @Test
+  public void test() {
+    assertEquals(
+        2,
+        solution.threeSumClosest(new int[] {-1, 2, 1, -4}, 1)
+    );
+
+  }
+
+}
