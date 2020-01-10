83fc03f5d3bd17a75535e7fc5bfafdf0f902119a
lu.zhou <lu.zhou@workday.com>
2020-01-10 09:46:57 -0800

    update 0002 python2

--- python2/l0002_add_two_numbers.py
+++ python2/l0002_add_two_numbers.py
@@ -36,14 +36,7 @@ class Solution(object):
             p.next = ListNode(c)
         return dummy.next
         
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
+    def addTwoNumbers2(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
--- python2/test_l0002_add_two_numbers.py
+++ python2/test_l0002_add_two_numbers.py
@@ -10,3 +10,9 @@ class TestRestore(unittest.TestCase):
         n2 = common.create_linked_list([5, 6, 4])
         n3 = l0002_add_two_numbers.Solution().addTwoNumbers(n1, n2)
         self.assertEqual([7, 0, 8], common.convert_linked_list_to_list(n3))
+
+    def test_solution2(self):
+        n1 = common.create_linked_list([2, 4, 3])
+        n2 = common.create_linked_list([5, 6, 4])
+        n3 = l0002_add_two_numbers.Solution().addTwoNumbers2(n1, n2)
+        self.assertEqual([7, 0, 8], common.convert_linked_list_to_list(n3))
