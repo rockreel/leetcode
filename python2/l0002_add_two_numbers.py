03715fceabf4fdb76ab7b38e6d1d9c11123e177b
lu.zhou <lu.zhou@workday.com>
2020-01-09 16:25:48 -0800

    add test python 0002

--- python2/l0002_add_two_numbers.py
+++ python2/l0002_add_two_numbers.py
@@ -4,6 +4,8 @@
 #         self.val = x
 #         self.next = None
 
+from common import ListNode
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -77,26 +79,3 @@ class Solution(object):
             p.next = ListNode(1)
             
         return r.next
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        head = ListNode(None)
-        r, h1, h2 = head, l1, l2
-        carry = 0
-        while h1 or h2 or carry:
-            v1 = h1.val if h1 else 0
-            v2 = h2.val if h2 else 0
-            v = v1 + v2 + carry
-            carry = v // 10
-            r.next = ListNode(v % 10)
-            r = r.next
-            h1 = h1.next if h1 else None
-            h2 = h2.next if h2 else None
-        
-        return head.next
---
+++ python2/test_l0002_add_two_numbers.py
@@ -0,0 +1,12 @@
+import unittest
+
+import common
+import l0002_add_two_numbers
+
+class TestRestore(unittest.TestCase):
+    
+    def test_solution(self):
+        n1 = common.create_linked_list([2, 4, 3])
+        n2 = common.create_linked_list([5, 6, 4])
+        n3 = l0002_add_two_numbers.Solution().addTwoNumbers(n1, n2)
+        self.assertEqual([7, 0, 8], common.convert_linked_list_to_list(n3))
