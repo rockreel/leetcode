2fdcf378d138b1fd0ecd5797160bbac6d596c328
intmonster <zhoulv82@gmail.com>
2020-04-13 13:26:32 -0700

    84

--- python3/l0084_largest_rectangle_in_histogram.py
+++ python3/l0084_largest_rectangle_in_histogram.py
@@ -5,13 +5,13 @@ class Solution:
         stack = [(-1, -1)]  # Stack of (index, height).
         max_area = 0
         for i, h in enumerate(heights):
-            while stack[-1][1] > h:
-                j, _ = stack.pop()
-                max_area = max(max_area, (j - stack[-1][0] + 1) * stack[-1][1])
+            while stack[-1][1] >= h:
+                _, hj = stack.pop()
+                max_area = max(max_area, (i - stack[-1][0] - 1) * hj)
             stack.append((i, h))
 
         while len(stack) > 1:
-            j, _ = stack.pop()
-            max_area = max(max_area, (j - stack[-1][0] + 1) * stack[-1][1])
+            _, hj = stack.pop()
+            max_area = max(max_area, (len(heights) - stack[-1][0] - 1) * hj)
         
         return max_area
