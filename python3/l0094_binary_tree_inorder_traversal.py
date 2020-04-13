2a85360d1eef50d08c44378810dc298e734c4686
intmonster <zhoulv82@gmail.com>
2020-04-13 11:02:39 -0700

    94

--- python3/l0094_binary_tree_inorder_traversal.py
+++ python3/l0094_binary_tree_inorder_traversal.py
@@ -3,7 +3,23 @@ from typing import List
 
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
+        nodes = []
+        stack = []
+        current = root
+        while current or stack:
+            if current:
+                stack.append(current)
+                current = current.left
+            else:
+                if stack:
+                    node = stack.pop()
+                    nodes.append(node.val)
+                    if node.right:
+                        current = node.right
+        return nodes
 
+
+    def inorderTraversalRecursive(self, root: TreeNode) -> List[int]:
         def inorder_traverse(root: TreeNode, nodes: List[int]):
             if not root:
                 return
