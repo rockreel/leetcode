d88ea6a361cff511208940596c3475c640e89f61
intmonster <zhoulv82@gmail.com>
2020-04-16 11:31:05 -0700

    127

--- python3/l0127_word_letter.py
+++ python3/l0127_word_letter.py
@@ -16,22 +16,21 @@ class Solution:
         # Initialize word graph.
         wordList.append(beginWord)
         word_graph = defaultdict(list)
-        for i in range(len(wordList)):
-            for j in range(i+1, len(wordList)):
-                if word_adjacent(wordList[i], wordList[j]):
-                    word_graph[wordList[i]].append(wordList[j])
-                    word_graph[wordList[j]].append(wordList[i])
+        for word in wordList:
+            for i in range(len(word)):
+                word_graph[word[:i] + '*' + word[i+1:]].append(word)
 
         # BFS to find minimum steps.
         queue = [(beginWord, 1)]
         visited = set([beginWord])
         while queue:
             word, length = queue.pop(0)
-            for w in word_graph[word]:
-                if w == endWord:
-                    return length + 1
-                if w not in visited:
-                    queue.append((w, length + 1))
-                    visited.add(w)
+            for i in range(len(word)):
+                for w in word_graph[word[:i] + '*' + word[i+1:]]:
+                    if w == endWord:
+                        return length + 1
+                    if w not in visited:
+                        queue.append((w, length + 1))
+                        visited.add(w)
 
         return 0
