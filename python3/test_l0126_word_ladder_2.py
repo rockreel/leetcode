c671d2c9296ae26e0daac82a0b5a883055ba25ce
intmonster <zhoulv82@gmail.com>
2020-04-16 14:31:33 -0700

    126

--- python3/test_l0126_word_ladder_2.py
+++ python3/test_l0126_word_ladder_2.py
@@ -5,15 +5,15 @@ from l0126_word_letter_2 import Solution
 class Test(unittest.TestCase):
     
     def test_solution(self):
-        # self.assertEqual(
-        #     sorted([
-        #         ['hit', 'hot', 'dot', 'dog', 'cog'],
-        #         ['hit', 'hot', 'lot', 'log', 'cog']
-        #     ]), 
-        #     sorted(Solution().findLadders('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog'])))
-        # self.assertEqual(
-        #     sorted([]),
-        #     sorted(Solution().findLadders('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log'])))
+        self.assertEqual(
+            sorted([
+                ['hit', 'hot', 'dot', 'dog', 'cog'],
+                ['hit', 'hot', 'lot', 'log', 'cog']
+            ]), 
+            sorted(Solution().findLadders('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog'])))
+        self.assertEqual(
+            sorted([]),
+            sorted(Solution().findLadders('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log'])))
         self.assertEqual(
             sorted([
                 ['red', 'ted', 'tad', 'tax'],
