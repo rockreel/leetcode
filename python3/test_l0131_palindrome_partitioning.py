533709460f8f2d2b32f3fbcf90a5dc0bb0807017
intmonster <zhoulv82@gmail.com>
2020-04-16 17:07:50 -0700

    131

---
+++ python3/test_l0131_palindrome_partitioning.py
@@ -0,0 +1,11 @@
+import unittest
+
+from l0131_palindrome_partitioning import Solution
+
+class Test(unittest.TestCase):
+    
+    def test_solution(self):
+        self.assertEqual(
+            sorted([['aa', 'b'], ['a', 'a', 'b']]),
+            sorted(Solution().partition('aab'))
+        )
