4884c93037b07848104ea27dfdb7bd67ced14fd9
intmonster <zhoulv82@gmail.com>
2020-04-19 13:33:01 -0700

    198

---
+++ python3/l0198_house_robber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # dp[i]: max money can rob from nums[:i].
+        dp = [0] * (len(nums) + 1)
+        for i in range(1, len(nums) + 1):
+            dp[i] = max(dp[i-1], nums[i-1] + dp[i-2])
+            
+        return dp[-1]
+
