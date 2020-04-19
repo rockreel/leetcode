8c8939093f55b9c4620594a677e40b35e7c6c94c
intmonster <zhoulv82@gmail.com>
2020-04-19 14:37:42 -0700

    179

---
+++ python3/l0179_largest_number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        def compare(s1: str, s2: str) -> int:
+            if s1 + s2 > s2 + s1:
+                return -1
+            else:
+                return 1
+            
+        number = ''.join(sorted([str(n) for n in nums], key=cmp_to_key(compare)))
+
+        return number if number[0] != '0' else '0'
