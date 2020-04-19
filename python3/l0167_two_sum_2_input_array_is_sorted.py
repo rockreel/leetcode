b19e2b7f707f9c4d73ecc5b2d06dc79706be21e5
intmonster <zhoulv82@gmail.com>
2020-04-18 20:37:26 -0700

    167

---
+++ python3/l0167_two_sum_2_input_array_is_sorted.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            sum_ = numbers[i] + numbers[j]
+            if sum_ == target:
+                return [i + 1, j + 1]
+            elif sum_ > target:
+                j -= 1
+            else:
+                i += 1
+        
