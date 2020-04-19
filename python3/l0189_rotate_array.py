e7cb7103cdfb2d24a5fd9a342f91f5bb678ee000
intmonster <zhoulv82@gmail.com>
2020-04-19 12:15:48 -0700

    189

---
+++ python3/l0189_rotate_array.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[::-1]
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
+        
