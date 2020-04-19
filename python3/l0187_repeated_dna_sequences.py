c3adaf2609c6d8d15d6f77d74819cb563edad919
intmonster <zhoulv82@gmail.com>
2020-04-19 14:51:53 -0700

    187

---
+++ python3/l0187_repeated_dna_sequences.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        existing = set()
+        repeated = set()
+        for i in range(len(s)-9):
+            seq = s[i:i+10]
+            if seq in existing:
+                repeated.add(seq)
+            else:
+                existing.add(seq)
+        return list(repeated)
