89f4fd6b5a1cb4b32479883d4966bf798b59811f
intmonster <zhoulv82@gmail.com>
2020-04-25 09:40:52 -0700

    235

---
+++ python3/l0235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,14 @@
+from common import TreeNode
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or not p or not q:
+            return None
+        
+        if p.val <= root.val <= q.val or p.val >= root.val >= q.val:
+            return root
+        elif root.val > p.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return self.lowestCommonAncestor(root.right, p, q)
+
