38e59ef511c82418ee10871edb12d92a5ec99232
intmonster <zhoulv82@gmail.com>
2020-04-25 14:18:44 -0700

    240

---
+++ python3/l0240_search_a_2d_matrix_2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        i = len(matrix) - 1
+        j = 0
+        while i >= 0 and j < len(matrix[0]):
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                i -= 1
+            else:
+                j += 1
+        return False
