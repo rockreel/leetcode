a36365763f42989b3f40f81cf767a26721af510b
intmonster <zhoulv82@gmail.com>
2020-04-27 14:45:47 -0700

    224

--- python3/l0224_basic_calculator.py
+++ python3/l0224_basic_calculator.py
@@ -1,15 +1,41 @@
-class Solution:
-    def calculate(self, s: str) -> int:
+from typing import List
 
-        def calculate_no_paren(s: str) -> int:
-            return 0
+class Solution:
+
+    def calculate(self, s: str) -> int:
+        
+        def calculate_stack(stack: List[int]) -> int:
+            # Calculate result up to a ( or entire stack.
+            r = 0
+            while stack and stack[-1] != '(':
+                num = stack.pop()
+                op = stack.pop() if stack and stack[-1] != '(' else '+'
+                if op == '+':
+                    r += num
+                else:
+                    r -= num
+            return r
 
         stack = []
-        for i, c in s:
+        curr_num = ''
+        for c in s:
+            if c == ' ':
+                continue
             if c == '(':
-                stack.append(i)
+                stack.append(c)
+            elif c == ')':
+                stack.append(int(curr_num))
+                # Calculate result back to last '('.
+                r = calculate_stack(stack)
+                # Pop out last '('.
+                stack.pop()
+                curr_num = str(r)
+            elif c in '+-':
+                stack.append(int(curr_num))
+                stack.append(c)
+                curr_num = ''
             else:
-                j = stack.pop()
-                calculate_no_paren(s[j+1:i])
-        return 0
-       
+                curr_num = curr_num + c
+        stack.append(int(curr_num))
+
+        return calculate_stack(stack)
--- python3/test_l0224_basic_calculator.py
+++ python3/test_l0224_basic_calculator.py
@@ -5,7 +5,6 @@ from l0224_basic_calculator import Solution
 class Test(unittest.TestCase):
     
     def test_solution(self):
-        pass
         # self.assertEqual(2, Solution().calculate('1 + 1'))
         # self.assertEqual(3, Solution().calculate(' 2-1 + 2 '))
-        # self.assertEqual(23, Solution().calculate('(1+(4+5+2)-3)+(6+8)'))
+        self.assertEqual(23, Solution().calculate('(1+(4+5+2)-3)+(6+8)'))
