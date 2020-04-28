f8ad1d5f1bc28f512f8fd8c2d21300c8bdd7dfe7
intmonster <zhoulv82@gmail.com>
2020-04-28 16:09:09 -0700

    248

--- python3/l0248_strobogrammatic_number_3.py
+++ python3/l0248_strobogrammatic_number_3.py
@@ -5,35 +5,32 @@ class Solution:
     def strobogrammaticInRange(self, low: str, high: str) -> int:
         # write your code here
 
-        def generate_strobogrammatic(n: int, n_low: int, n_high: int, nums_in_range: List[str]):
+        def generate_strobogrammatic(n: int, low: str, high: str, result: List[int]):
             num_map = {
                 '1': '1', '6': '9', '8': '8',
                 '9': '6', '0': '0'
             }
             nums = []
             if n == 0:
-                nums = ['']
+                return ['']
             elif n == 1:
                 nums = [k for k in num_map.keys() if k not in ['6', '9']]
             else:
-                for num in generate_strobogrammatic(n-2, n_low, n_high, nums_in_range):
+                for num in generate_strobogrammatic(n-2, low, high, result):
                     for k, v in num_map.items():
                         nums.append(k + num + v)
-            if n_low <= n <= n_high:
-                nums_in_range.extend(nums)
+            
+            if len(low) <= n <= len(high):
+                nums_valid = [n for n in nums if (len(n) == 1 or n[0] != '0')]
+                if n == len(low):
+                    nums_valid = [n for n in nums_valid if n >= low]
+                if n == len(high):
+                    nums_valid = [n for n in nums_valid if n <= high]
+                result[0] += len(nums_valid)
             return nums
 
-        nums_in_range = []
-        generate_strobogrammatic(len(high), len(low), len(high), nums_in_range)
-        generate_strobogrammatic(len(high) - 1, len(low), len(high), nums_in_range)
-        nums_filtered = []
-        for n in nums_in_range:
-            if len(n) > 1 and n[0] == '0':
-                continue
-            if len(n) == len(low) and n < low:
-                continue
-            elif len(n) == len(high) and n > high:
-                continue
-            nums_filtered.append(n)
-        return len(nums_filtered)
+        result = [0]
+        generate_strobogrammatic(len(high), low, high, result)
+        generate_strobogrammatic(len(high) - 1, low, high, result)
+        return result[0]
 
