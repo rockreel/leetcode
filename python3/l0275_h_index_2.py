34746a2843430ae3e9314b2c945208e119e8d993
intmonster <zhoulv82@gmail.com>
2020-04-29 00:29:41 -0700

    275

---
+++ python3/l0275_h_index_2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations = citations[::-1]
+        start, end = 0, len(citations) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            h = mid + 1
+            if h == citations[mid]:
+                return h
+            elif h > citations[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        
+       
+        return start
+
