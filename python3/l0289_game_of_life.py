d6e90e1e694ac54b47e7c9a4c5d718b984ed0dbc
intmonster <zhoulv82@gmail.com>
2020-04-29 20:33:45 -0700

    289

---
+++ python3/l0289_game_of_life.py
@@ -0,0 +1,41 @@
+class Solution:
+
+
+
+
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                live = 0
+                if i - 1 >= 0 and board[i-1][j] > 0:
+                    live += 1
+                if i + 1 < len(board) and board[i+1][j] > 0:
+                    live += 1
+                if j - 1 >= 0 and board[i][j-1] > 0:
+                    live += 1
+                if j + 1 < len(board[0]) and board[i][j+1] > 0:
+                    live += 1
+                if i - 1 >= 0 and j - 1 >= 0 and board[i-1][j-1] > 0:
+                    live += 1
+                if i + 1 < len(board) and j + 1 < len(board[0]) and board[i+1][j+1] > 0:
+                    live += 1
+                if i + 1 < len(board) and j - 1 >= 0 and board[i+1][j-1] > 0:
+                    live += 1
+                if i - 1 >= 0 and j + 1 < len(board[0]) and board[i-1][j+1] > 0:
+                    live += 1
+                if board[i][j] == 1:
+                    if live < 2 or live > 3:
+                        board[i][j] = 2
+                else:
+                    if live == 3:
+                        board[i][j] = -1
+                        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 2:
+                    board[i][j] = 0
+                elif board[i][j] == -1:
+                    board[i][j] = 1
