41d1801625064d986ddf7363d1177c2abc394754
intmonster <zhoulv82@gmail.com>
2020-05-08 20:08:37 -0700

    315

---
+++ python3/l0315_count_of_smaller_numbers_after_self.py
@@ -0,0 +1,11 @@
+import bisect
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        nums_sorted = []
+        result = []
+        for i in range(len(nums)-1, -1, -1):
+            pos = bisect.bisect_left(nums_sorted, nums[i])
+            result.append(pos)
+            nums_sorted.insert(pos, nums[i])
+        return result[::-1]
