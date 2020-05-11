2d30196165686ea74d3b6f8fa787a86a47ce4b3d
intmonster <zhoulv82@gmail.com>
2020-05-10 21:01:38 -0700

    update

--- python3/l0330_patching_array.py
+++ python3/l0330_patching_array.py
@@ -7,9 +7,13 @@ class Solution:
         i = 0
         while sum_to <= n:
             if i < len(nums) and nums[i] <= sum_to:
+                # If there is num less or equal to sum_to, then
+                # sum_to can be extended to sum_to + num[i].
                 sum_to += nums[i]
                 i += 1
             else:
+                # If need to add a number, then add sum_to to array
+                # so range can be extended most.
                 sum_to += sum_to
                 added += 1
         return added
