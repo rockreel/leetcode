7efa86b25d1d8f6c6334ac09c1e123cb9d22f203
intmonster <zhoulv82@gmail.com>
2020-05-11 21:35:15 -0700

    332

---
+++ python3/l0332_reconstruct_itinerary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        def find_itinerary(start, tickets) -> List[str]:
+            if not tickets:
+                return [[start]]
+                
+            for i, t in enumerate(tickets):
+                if t[0] != start:
+                    continue
+                for it in find_itinerary(t[1], tickets[:i]+tickets[i+1:]):
+                    return [[start] + it]
+            return []
+            
+        tickets = sorted(tickets)
+        return find_itinerary('JFK', tickets)[0]
+
