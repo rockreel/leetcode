628660758dd5c8e85d925d2a731caf3612686c51
intmonster <zhoulv82@gmail.com>
2020-05-13 15:22:50 -0700

    test

--- python3/test_l0341_flatten_nested_list_iterator.py
+++ python3/test_l0341_flatten_nested_list_iterator.py
@@ -16,3 +16,11 @@ class Test(unittest.TestCase):
         self.assertEqual(1, nit.next())
         self.assertEqual(1, nit.next())
         self.assertEqual(False, nit.hasNext())
+
+        nit = NestedIterator([
+            NestedInteger(1),
+            NestedInteger([NestedInteger(4), NestedInteger([NestedInteger(6)])])])
+        self.assertEqual(1, nit.next())
+        self.assertEqual(4, nit.next())
+        self.assertEqual(6, nit.next())
+        self.assertEqual(False, nit.hasNext())
