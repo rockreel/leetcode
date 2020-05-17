bfef3e358f53a461784cdc7ef341d13399314ff7
intmonster <zhoulv82@gmail.com>
2020-05-17 10:16:35 -0700

    test

--- python3/test_l0990_satisfiability_of_equality_equations.py
+++ python3/test_l0990_satisfiability_of_equality_equations.py
@@ -1,6 +1,28 @@
 import unittest
 
+from l0990_satisfiability_of_equality_equations import Solution
+
 class Test(unittest.TestCase):
     
     def test_solution(self):
-        self.assertEqual(1, 1)
+        self.assertEqual(
+            False,
+            Solution().equationsPossible(
+                ['a==b', 'b!=a']))
+        self.assertEqual(
+            True,
+            Solution().equationsPossible(
+                ['a==b', 'b==a']))
+        self.assertEqual(
+            True,
+            Solution().equationsPossible(
+                ['a==b', 'b==c', 'a==c']))
+        self.assertEqual(
+            False,
+            Solution().equationsPossible(
+                ['a==b', 'b!=c', 'c==a']))
+        self.assertEqual(
+            True,
+            Solution().equationsPossible(
+                ['c==c', 'b==d', 'x!=z']))
+
