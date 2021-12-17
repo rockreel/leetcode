ef717f0149e4df8e0af2835dc5e6c4b7e16c3c70
lu.zhou <lu.zhou@workday.com>
2021-12-17 10:03:15 -0800

    154

--- python3/l0154_find_minimum_in_rotated_sorted_array_2.py
+++ python3/l0154_find_minimum_in_rotated_sorted_array_2.py
@@ -2,6 +2,23 @@ from typing import List
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        start, end = 0, len(nums) - 1
+        while start < end:
+            if nums[start] < nums[end]:
+                return nums[start]
+            elif nums[start] == nums[end]:
+                while end - 1 >= 0 and nums[end-1] == nums[end]:
+                    end -= 1
+                end -= 1
+            else:
+                middle = start + (end - start) // 2
+                if nums[middle] >= nums[start]:
+                    start = middle + 1
+                elif nums[middle] < nums[start]:
+                    end = middle
+        return nums[start]
+
+    def findMin2(self, nums: List[int]) -> int:
         start, end = 0, len(nums) - 1      
         while start + 1 < end:
             if nums[start] < nums[end]:
