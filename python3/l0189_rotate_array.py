82dcc2f1d0556d73fdb572ad8125f481052540d1
lu.zhou <lu.zhou@workday.com>
2022-01-19 22:14:38 -0800

    189

--- python3/l0189_rotate_array.py
+++ python3/l0189_rotate_array.py
@@ -5,8 +5,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        def reverse(nums, start, end):
+            i, j = start, end
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+
         k = k % len(nums)
-        nums[:] = nums[::-1]
-        nums[:k] = nums[:k][::-1]
-        nums[k:] = nums[k:][::-1]
+        reverse(nums, 0, len(nums) - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, len(nums) - 1)
         
