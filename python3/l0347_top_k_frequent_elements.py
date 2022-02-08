967f6fb61165ebaa79b92c48f7cf03c1d62ef9fe
lu.zhou <lu.zhou@workday.com>
2022-02-08 15:05:33 -0800

    347

--- python3/l0347_top_k_frequent_elements.py
+++ python3/l0347_top_k_frequent_elements.py
@@ -5,5 +5,4 @@ from typing import List
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         num_count = Counter(nums)
-        heap = []
         return [n for _, n in heapq.nlargest(k, [(c, n) for n, c in num_count.items()])]
