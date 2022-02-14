e4964ae094fe6b70a27509e6d21702f664f55d6c
lu.zhou <lu.zhou@workday.com>
2022-02-13 22:54:40 -0800

    188

--- python3/l0188_best_time_to_buy_and_sell_stock_4.py
+++ python3/l0188_best_time_to_buy_and_sell_stock_4.py
@@ -2,30 +2,11 @@ from typing import List
 
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        if k >= len(prices) // 2:
-            # Trades more than prices, just sum every price up.
-            max_profit = 0
-            for i in range(len(prices)-1):
-                max_profit += max(prices[i+1] - prices[i], 0)
-            return max_profit
-
-        # For trades less than prices use dynamic programing.
-        # dp[i][j]: max profit with i trades on prices[:j]
-        dp = [[0 for _ in range(len(prices)+1)] for _ in range(k+1)]
-
-        for i in range(1, min(k + 1, len(prices) // 2 + 2)):
-            max_protential_profit = float('-inf')
-            for j in range(2, len(prices)+1):
-                max_protential_profit = max(
-                    dp[i-1][j-2] - prices[j-2],  # After profit before prices[j-2], buy at prices[j-2]
-                    max_protential_profit
-                )
-                dp[i][j] = max(
-                    max_protential_profit + prices[j-1],  # Sell at prices[j-1]
-                    dp[i][j-1]  # No trade on prices[j-1]
-                )
-                # For reference only.
-                # for l in range(j-1):
-                #     max_profit =  max(max_profit, dp[i-1][l] - prices[l])
-                
-        return dp[-1][-1]
+        # dp[i][j]: max profit at ith day with j transaction remaining, hold or not hold a stock.
+        dp_hold = [[0] * (k+1) for _ in range(len(prices)+1)]
+        dp_not_hold = [[0] * (k+1) for _ in range(len(prices)+1)]
+        for i in range(len(prices)-1, -1, -1):
+            for j in range(1, k+1):
+                dp_hold[i][j] = max(dp_hold[i+1][j], prices[i] + dp_not_hold[i+1][j-1])
+                dp_not_hold[i][j] = max(dp_not_hold[i+1][j], -prices[i] + dp_hold[i+1][j])
+        return dp_not_hold[0][k]
