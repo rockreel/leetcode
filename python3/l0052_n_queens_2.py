a7ba6e40e0e85207888fede11e97aba3f5d3d1b9
lu.zhou <lu.zhou@workday.com>
2022-06-06 11:28:31 -0700

    0052

--- python3/l0052_n_queens_2.py
+++ python3/l0052_n_queens_2.py
@@ -32,3 +32,34 @@ class Solution:
         queens = []
         solve(queens, n, solution)
         return len(solution)
+
+    def totalNQueens2(self, n: int) -> int:
+        def canAttack(q1, q2):
+            if q1[0] == q2[0]:
+                return True
+            if q1[1] == q2[1]:
+                return True
+            if abs(q1[0]-q2[0]) == abs(q1[1]-q2[1]):
+                return True
+            return False
+        
+        def placeQueen(queens, row, n, result):
+            if len(queens) == n:
+                result.append(queens)
+                return
+            
+            for i in range(n):
+                can_attack = False
+                for q in queens:
+                    if canAttack(q, (row, i)):
+                        can_attack = True
+                        break
+                if not can_attack:
+                    queens.append((row, i))
+                    placeQueen(queens, row + 1, n, result)
+                    queens.pop()
+            return 
+        
+        result = []
+        placeQueen([], 0, n, result)
+        return len(result)
