146dbb09aa85ec58c6432889fad86218ac24fe86
lu.zhou <lu.zhou@workday.com>
2022-06-18 11:42:59 -0700

    404

--- python3/l0404_sum_of_left_leaves.py
+++ python3/l0404_sum_of_left_leaves.py
@@ -13,3 +13,18 @@ class Solution:
                     return 0
             return sum_left_leaf(root.left, True) + sum_left_leaf(root.right, False)
         return sum_left_leaf(root, False)
+
+    def sumOfLeftLeavesIterative(self, root: Optional[TreeNode]) -> int:
+        stack = []
+        if root:
+            stack.append((root, False))
+        result = 0
+        while stack:
+            node, is_left_child = stack.pop()
+            if is_left_child and not node.left and not node.right:
+                result += node.val
+            if node.right:
+                stack.append((node.right, False))
+            if node.left:
+                stack.append((node.left, True))
+        return result
