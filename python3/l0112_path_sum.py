0805622bd39d20bc2b240b3f3bd0dca518ce4138
lu.zhou <lu.zhou@workday.com>
2022-06-19 20:25:18 -0700

    112

--- python3/l0112_path_sum.py
+++ python3/l0112_path_sum.py
@@ -1,4 +1,5 @@
 from common import TreeNode
+from typing import Optional
 
 class Solution:
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
@@ -14,5 +15,19 @@ class Solution:
         if self.hasPathSum(root.right, sum - root.val):
             return True
 
+        return False
+
+    def hasPathSumIterative(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        queue = []
+        if root:
+            queue.append((root, targetSum))
+        while queue:
+            node, target = queue.pop(0)
+            if not node.left and not node.right and node.val == target:
+                return True
+            if node.left:
+                queue.append((node.left, target - node.val))
+            if node.right:
+                queue.append((node.right, target - node.val))
         return False
         
