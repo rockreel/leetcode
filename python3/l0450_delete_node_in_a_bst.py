41a2f861d9ea49dd4c69e79748ff7e55b1c73733
lu.zhou <lu.zhou@workday.com>
2022-06-20 17:20:10 -0700

    450

--- python3/l0450_delete_node_in_a_bst.py
+++ python3/l0450_delete_node_in_a_bst.py
@@ -2,7 +2,7 @@ from typing import Optional
 from common import TreeNode
 
 class Solution:
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+    def deleteNodeSetNodeValue(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return None
         
@@ -22,6 +22,30 @@ class Solution:
             root.val = min_value
             root.right = self.deleteNode(root.right, min_value)
             return root
+        else:
+            if root.val > key:
+                root.left = self.deleteNode(root.left, key)
+            else:
+                root.right = self.deleteNode(root.right, key)
+            return root
+    
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if root.val == key:
+            if not root.left and not root.right:
+                return None
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+            # Find minimum value on root.right and set root value to it.
+            min_node = root.right
+            while min_node.left:
+                min_node = min_node.left
+            min_node.left = root.left
+            return root.right
         else:
             if root.val > key:
                 root.left = self.deleteNode(root.left, key)
