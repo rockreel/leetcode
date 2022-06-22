208aed51b7b72191223f48fa797c6c46e362558a
lu.zhou <lu.zhou@workday.com>
2022-06-21 21:42:20 -0700

    40

--- python3/l0040_combination_sum_2.py
+++ python3/l0040_combination_sum_2.py
@@ -17,3 +17,22 @@ class Solution:
             return result
         candidates = sorted(candidates)
         return combinationSumImpl(candidates, target)
+
+    def combinationSum2GlobalResult(self, candidates: List[int], target: int) -> List[List[int]]:
+        def combination(candidates, target, nums, result):
+            if target == 0:
+                result.append(nums)
+                return
+            if target < 0:
+                return
+            if not candidates:
+                return
+            for i, c in enumerate(candidates):
+                if i > 0 and candidates[i] == candidates[i-1]:
+                    continue
+                combination(candidates[i+1:], target - c, [c] + nums, result)
+            return
+        result = []
+        candidates = sorted(candidates)
+        combination(candidates, target, [], result)
+        return result
