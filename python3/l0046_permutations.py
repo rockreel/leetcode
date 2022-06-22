5cfc776e2d3982046c6104bb90d9166448c00975
lu.zhou <lu.zhou@workday.com>
2022-06-21 22:16:46 -0700

    permutation

--- python3/l0046_permutations.py
+++ python3/l0046_permutations.py
@@ -9,3 +9,20 @@ class Solution:
             for r in self.permute(nums[:i] + nums[i+1:]):
                 result.append([n] + r)
         return result
+
+    def permuteIndex(self, nums: List[int]) -> List[List[int]]:
+        def find_permutation(nums, perm, result, used):
+            if len(perm) == len(nums):
+                result.append(perm)
+                return
+            for i in range(len(nums)):
+                if nums[i] in used:
+                    continue
+                used.add(nums[i])
+                find_permutation(nums, perm + [nums[i]], result, used)
+                used.remove(nums[i])
+                
+            return result
+        result = []
+        find_permutation(nums, [], result, set([]))
+        return result
--- python3/l0047_permutations_2.py
+++ python3/l0047_permutations_2.py
@@ -15,3 +15,24 @@ class Solution:
                 prev = n
             return result
         return permuteUniqueImpl(sorted(nums))
+
+    def permuteUniqueIndex(self, nums: List[int]) -> List[List[int]]:
+        def find_permutation(nums, perm, result, used_on_branch):
+            if len(perm) == len(nums):
+                result.append(perm)
+                return
+            used_on_level = set([])
+            for i in range(len(nums)):
+                if nums[i] in used_on_level:
+                    continue
+                if i in used_on_branch:
+                    continue
+                used_on_level.add(nums[i])
+                used_on_branch.add(i)
+                find_permutation(nums, perm + [nums[i]], result, used_on_branch)
+                used_on_branch.remove(i)
+                
+            return result
+        result = []
+        find_permutation(sorted(nums), [], result, set([]))
+        return result
