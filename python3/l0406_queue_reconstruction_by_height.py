27a9421e5ace52007b42a9326980dbc8bedf33bb
lu.zhou <lu.zhou@workday.com>
2022-06-22 22:39:17 -0700

    406

--- python3/l0406_queue_reconstruction_by_height.py
+++ python3/l0406_queue_reconstruction_by_height.py
@@ -1,9 +1,10 @@
 from typing import List
+from collections import deque
 
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         people = sorted(people, key=lambda p: (-p[0], p[1]))
-        queue = []
+        queue = deque()
         for p in people:
             queue.insert(p[1], p)
         return queue
