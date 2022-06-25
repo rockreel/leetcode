52d6bb9235fc7565576a0653698fb55ba97323c6
lu.zhou <lu.zhou@workday.com>
2022-06-25 11:44:22 -0700

    494

--- python3/l0494_target_sum.py
+++ python3/l0494_target_sum.py
@@ -53,3 +53,27 @@ class Solution:
             print(dp)
             
         return dp[s+target] if target <= s else 0
+
+    def findTargetSumWaysKnapsack(self, nums: List[int], target: int) -> int:
+        # A + B = sum(nums)
+        # A - B = target
+        # => A = (sum(nums) + target) // 2
+        target = sum(nums) + target
+        if target % 2 != 0:
+            return 0
+        target = target // 2
+        if target < 0:
+            return 0
+        dp = [[0] * (target + 1) for _ in nums]
+        
+        for i in range(len(dp)):
+            dp[i][0] = 1
+        for j in range(len(dp[0])):
+            if nums[0] == j:
+                dp[0][j] += 1
+
+        for i in range(1, len(dp)):
+            for j in range(0, len(dp[0])):
+                dp[i][j] = dp[i-1][j] + (dp[i-1][j-nums[i]] if j -nums[i] >= 0 else 0)
+
+        return dp[-1][-1]
