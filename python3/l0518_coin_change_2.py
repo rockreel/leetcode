4668fe480d854cd1c1608c5a50370376c46f6ee0
lu.zhou <lu.zhou@workday.com>
2022-07-03 10:56:41 -0700

    518

--- python3/l0518_coin_change_2.py
+++ python3/l0518_coin_change_2.py
@@ -2,14 +2,16 @@ from typing import List
 
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # dp[i][j]: with coins[:i] coins and j amount.
-        dp = [[0] * (amount+1) for _ in range(len(coins)+1)]
-        for i in range(len(coins)+1):
+        # dp[i][j]: with coins[:i+1] coins and j amount.
+        dp = [[0] * (amount + 1) for _ in coins]
+        for i in range(len(dp)):
             dp[i][0] = 1
-        for i in range(1, len(coins)+1):
-            for j in range(1, amount+1):
-                dp[i][j] = dp[i-1][j]  # Without current indexed coin.
-                if j - coins[i-1] >= 0:
-                    # With current indexed coin.
-                    dp[i][j] += dp[i][j - coins[i-1]]    
+        for j in range(len(dp[0])):
+            if j % coins[0] == 0:
+                dp[0][j] = 1
+        for i in range(1, len(dp)):
+            for j in range(len(dp[0])):
+                dp[i][j] = dp[i-1][j]
+                if j-coins[i] >= 0:
+                    dp[i][j] += dp[i][j-coins[i]]
         return dp[-1][-1]
