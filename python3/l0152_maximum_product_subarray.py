11d2f2a9dd8b0e631b0c3050902885acaa1ef9a8
lu.zhou <lu.zhou@workday.com>
2022-07-17 11:53:41 -0700

    152

--- python3/l0152_maximum_product_subarray.py
+++ python3/l0152_maximum_product_subarray.py
@@ -1,7 +1,7 @@
 from typing import List
 
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
+    def maxProductBothDirection(self, nums: List[int]) -> int:
         max_product = float('-inf')
         product = 1
         for n in nums:
@@ -16,3 +16,16 @@ class Solution:
             if product == 0:
                 product = 1
         return max_product
+
+    def maxProduct(self, nums: List[int]) -> int:
+        max_product = nums[0]
+        min_product = nums[0]
+        result = max_product
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                max_product, min_product = min_product, max_product
+            max_product = max(nums[i], max_product * nums[i])
+            min_product = min(nums[i], min_product * nums[i])
+            result = max(max_product, result)
+        
+        return result
