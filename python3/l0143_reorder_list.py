1961f060ff318a0c0d0e56ed81b315017dc05920
lu.zhou <lu.zhou@workday.com>
2022-07-22 16:00:35 -0700

    143

--- python3/l0143_reorder_list.py
+++ python3/l0143_reorder_list.py
@@ -1,7 +1,7 @@
 from common import ListNode
 
 class Solution:
-    def reorderList(self, head: ListNode) -> None:
+    def reorderListStack(self, head: ListNode) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
@@ -22,3 +22,44 @@ class Solution:
         if p:
             p.next = None
         return
+
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # Find second part starting point.
+        if not head or not head.next:
+            return head
+        
+        fast, slow, prev = head, head, None
+        while fast and fast.next:
+            fast = fast.next.next
+            prev = slow
+            slow = slow.next
+        
+        prev.next = None    
+        
+        # Reverse the second part.
+        prev = None 
+        curr = slow
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        # Link together.
+        p1 = head
+        p2 = prev
+        dummy = ListNode(0)
+        p = dummy
+        while p1 and p2:
+            p.next = p1
+            p1 = p1.next
+            p = p.next
+            p.next = p2
+            p2 = p2.next
+            p = p.next
+        p.next = p1 if p1 else p2
+
+        return dummy.next
