aa8f74acec81aaccb5eff7c4095845c15735e6d4
lu.zhou <lu.zhou@workday.com>
2022-07-22 22:31:24 -0700

    79

--- python3/l0079_word_search.py
+++ python3/l0079_word_search.py
@@ -2,6 +2,33 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def search(board, word, row, col, visited):
+            if len(word) == 1 and board[row][col] == word:
+                return True
+            
+            if board[row][col] != word[0]:
+                return False
+            
+            visited.add((row, col))
+            if row > 0 and (row-1, col) not in visited and search(board, word[1:], row-1, col, visited):
+                return True
+            if row < len(board) - 1 and (row+1, col) not in visited and search(board, word[1:], row+1, col, visited):
+                return True
+            if col > 0 and (row, col-1) not in visited and search(board, word[1:], row, col-1, visited):
+                return True
+            if col < len(board[0]) - 1 and (row, col+1) not in visited and search(board, word[1:], row, col+1, visited):
+                return True
+            visited.remove((row, col))
+            return False
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if search(board, word, i, j, set([])):
+                    return True
+        return False
+    
+    def exist(self, board: List[List[str]], word: str) -> bool:
 
         def exist_at(board: List[List[str]], word: str, row: int, col: int) -> bool:
             if not word:
